aff377c692a137b773d4c58e042cc0cec3fc70af
45number <45number@gmail.com>
2026-06-22 00:26:08 +0400

    windows is fixed

--- lib/main.dart
+++ lib/main.dart
@@ -16,11 +16,14 @@ import 'core/database/settings_repository.dart';
 import 'core/database/word_repository.dart';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await Firebase.initializeApp();
+  // await Firebase.initializeApp();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   // Database
   database = AppDatabase();
